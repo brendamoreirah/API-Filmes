d6092991184bd8d82d110d973cf183855059fe9c
Brenda Moreira <brenda.m.moreira6@aluno.senai.br>
2025-02-20 09:07:51 -0300

    atualizando-API

---
+++ Domains/Genero.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_filmes_senai.Domains
+{
+    public class Genero
+    {
+
+        [Key]
+        public Guid IdGenero { get; set; }
+
+        [Column(TypeName = "VARCHAR(30)")]
+        [Required(ErrorMessage = "Nome do Genêro é obrigatório!")]
+        public string? Nome { get; set; }
+    }
+}
